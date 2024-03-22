310613278a102d61e2cdea50b1d1fd049405e000
Dylan Martin <dylanrossmartin@gmail.com>
2024-03-21 23:22:33 -0400

    Add

--- provisionAndLaunchCentralNode/src/run.ts
+++ provisionAndLaunchCentralNode/src/run.ts
@@ -1,6 +1,7 @@
 import { launchNode } from './launchNode'
 import { startRun } from './startRun'
 import path from 'path'
+import fs from 'fs'
 
 async function run() {
   const runId = Math.random().toString(36).substring(7)
@@ -17,8 +18,13 @@ async function run() {
     }),
   })
 
+  const commandsToRun = [
+    'bash',
+    '-c',
+    '/workspace/runKit/startup/start.sh && tail -f /dev/null',
+  ]
   // launch edge node for site 1
-  const hostDirectorySite1 = path.join(
+  const site1hostRunKitDirectory = path.join(
     __dirname,
     '../../basedir/runs/',
     consortiumId,
@@ -27,23 +33,46 @@ async function run() {
     userIds[0],
   )
 
-    const commandsToRun = ['bash', '-c', '/workspace/runKit/startup/start.sh && tail -f /dev/null']
+  const site1hostDataDirectory = path.join(
+    'C:/development/nvflare_app_boilerplate/test_data/site1',
+  )
+
+  const site1ResultsDirectory = path.join(
+    __dirname,
+    '../../basedir/runs/',
+    consortiumId,
+    runId,
+    'results',
+    userIds[0],
+  )
+  // make the directory if it doesn't exist
+  if (!fs.existsSync(site1ResultsDirectory)) {
+    fs.mkdirSync(site1ResultsDirectory, { recursive: true })
+  }
 
   await launchNode({
     containerService: 'docker',
     imageName: 'boilerplate_average_app',
     directoriesToMount: [
       {
-        hostDirectory: hostDirectorySite1,
+        hostDirectory: site1hostRunKitDirectory,
         containerDirectory: '/workspace/runKit/',
       },
+      {
+        hostDirectory: site1hostDataDirectory,
+        containerDirectory: '/workspace/data/',
+      },
+      {
+        hostDirectory: site1ResultsDirectory,
+        containerDirectory: '/workspace/results/',
+      }
     ],
     portBindings: [],
     commandsToRun: commandsToRun,
   })
 
   // launch edge node for site 2
-  const hostDirectorySite2 = path.join(
+  const site2hostRunKitDirectory = path.join(
     __dirname,
     '../../basedir/runs/',
     consortiumId,
@@ -52,14 +81,39 @@ async function run() {
     userIds[1],
   )
 
+  const site2hostDataDirectory = path.join(
+    'C:/development/nvflare_app_boilerplate/test_data/site2',
+  )
+
+  const site2ResultsDirectory = path.join(
+    __dirname,
+    '../../basedir/runs/',
+    consortiumId,
+    runId,
+    'results',
+    userIds[1],
+  )
+  // make the directory if it doesn't exist
+  if (!fs.existsSync(site2ResultsDirectory)) {
+    fs.mkdirSync(site2ResultsDirectory, { recursive: true })
+  }
+
   await launchNode({
     containerService: 'docker',
     imageName: 'boilerplate_average_app',
     directoriesToMount: [
       {
-        hostDirectory: hostDirectorySite2,
+        hostDirectory: site2hostRunKitDirectory,
         containerDirectory: '/workspace/runKit/',
       },
+      {
+        hostDirectory: site2hostDataDirectory,
+        containerDirectory: '/workspace/data/',
+      },
+      {
+        hostDirectory: site2ResultsDirectory,
+        containerDirectory: '/workspace/results/',
+      }
     ],
     portBindings: [],
     commandsToRun: commandsToRun,
