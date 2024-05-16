e282e0c7178903d371c4b76f609de204911542be
dylanmartin <dylanrossmartin@gmail.com>
2024-05-16 13:42:28 -0400

    update config and package.jsons

--- fileServer/src/config/defaultConfig.ts
+++ fileServer/src/config/defaultConfig.ts
@@ -1,6 +1,6 @@
 export default {
   authenticationUrl: 'http://localhost:4000/authenticateToken',
-  baseDir: 'C:\\development\\effective-palm-tree\\_devTestDirectories\\fileHostDir',
+  baseDir: '/Users/admin-dmartin99/development/effective-palm-tree/_devTestDirectories/fileHostDir',
   port: 4002,
   
 }
