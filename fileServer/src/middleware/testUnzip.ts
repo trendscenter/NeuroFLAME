f1a3ea24d892cc326859989b8f47d431193069c9
Dylan Martin <dylanrossmartin@gmail.com>
2024-07-16 15:01:30 -0400

    Add

--- fileServer/src/middleware/testUnzip.ts
+++ fileServer/src/middleware/testUnzip.ts
@@ -2,24 +2,29 @@ import { Request, Response, NextFunction } from 'express'
 import unzipper from 'unzipper'
 import path from 'path'
 import fs from 'fs'
+import getConfig from '../config/getConfig'
 
 interface MulterRequest extends Request {
   file: Express.Multer.File
   unzippedPath?: string
 }
 
+const config = await getConfig()
+
 const testUnzip = (req: MulterRequest, res: Response, next: NextFunction) => {
   const { consortiumId, runId } = req.params
   const filePath = req.file.path
-  const outputDir = path.join(__dirname, 'unzip_output', consortiumId, runId)
+
+  const { baseDir } = config
+  const extractPath = path.join(baseDir, consortiumId, runId)
 
   // Create output directory if it doesn't exist
-  fs.mkdirSync(outputDir, { recursive: true })
+  fs.mkdirSync(extractPath, { recursive: true })
 
   fs.createReadStream(filePath)
-    .pipe(unzipper.Extract({ path: outputDir }))
+    .pipe(unzipper.Extract({ path: extractPath }))
     .on('close', () => {
-      req.unzippedPath = outputDir
+      req.unzippedPath = extractPath
       next()
     })
     .on('error', (err: Error) => {
