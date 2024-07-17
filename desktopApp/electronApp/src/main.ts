a585a976e6c1af6b138a1bb6534ba6dc4b4f44e4
Dylan Martin <dylanrossmartin@gmail.com>
2024-07-17 15:17:25 -0400

    add

--- desktopApp/electronApp/src/main.ts
+++ desktopApp/electronApp/src/main.ts
@@ -25,7 +25,7 @@ async function createWindow(): Promise<void> {
 
   const appPath = app.getAppPath();
   const productionUrl = url.format({
-    pathname: path.join(appPath, 'build/index.html'),
+    pathname: path.join(appPath, '../app/build/index.html'),
     protocol: 'file:',
     slashes: true,
   })
