2f673f13fb5dd1d4802f3ff5744865244a7b6e74
Dylan Martin <dylanrossmartin@gmail.com>
2024-08-22 18:04:53 -0400

    change seed data

--- centralApi/src/database/computationNotesNvflareBoilerplate.ts
+++ centralApi/src/database/computationNotesNvflareBoilerplate.ts
@@ -1,12 +1,14 @@
 export default `
-- repositoryUrl: "https://github.com/dylanmartin/nvflare_app_boilerplate"
-- imageBuildSource: "https://github.com/dylanmartin/nvflare_app_boilerplate/tree/666334ce8c90bef70d6a27ff0e67a682a2c451ac"
 
+* **Repository URL**: [https://github.com/dylanmartin/nvflare_app_boilerplate](https://github.com/dylanmartin/nvflare_app_boilerplate)
+* **Image Build Source**: [https://github.com/dylanmartin/nvflare_app_boilerplate/tree/666334ce8c90bef70d6a27ff0e67a682a2c451ac](https://github.com/dylanmartin/nvflare_app_boilerplate/tree/666334ce8c90bef70d6a27ff0e67a682a2c451ac)
 
 # Example parameters
 
+\`\`\`json
 {
-    decimal_places: 10,
+    "decimal_places": 10
 }
+\`\`\`
 
 `
