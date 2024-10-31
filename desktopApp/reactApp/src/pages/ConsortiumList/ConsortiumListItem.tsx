0f151437fb85d581b1b9b9ae15698a1c38689bd2
Javier Romero <javiertomasromero@gmail.com>
2024-10-31 14:11:54 -0600

    Adding functions to Join/Leave Consortia

--- desktopApp/reactApp/src/pages/ConsortiumList/ConsortiumListItem.tsx
+++ desktopApp/reactApp/src/pages/ConsortiumList/ConsortiumListItem.tsx
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
 import { ListItem, ListItemText, Button, Box, Paper, Typography } from '@mui/material';
 import { ConsortiumListItem as ConsortiumListItemType } from '../../apis/centralApi/generated/graphql'; // Import the type
+import { useCentralApi } from "../../apis/centralApi/centralApi";
 import { useNavigate } from 'react-router-dom';
 import { useUserState } from '../../contexts/UserStateContext';
 
 interface ConsortiumListItemProps {
     consortium: ConsortiumListItemType;
+    onReload: any;
 }
 
-const ConsortiumListItem: React.FC<ConsortiumListItemProps> = ({ consortium }) => {
+const ConsortiumListItem: React.FC<ConsortiumListItemProps> = ({ consortium, onReload }) => {
     const navigate = useNavigate();
 
     const [loading, setLoading] = useState(false);
@@ -17,25 +19,18 @@ const ConsortiumListItem: React.FC<ConsortiumListItemProps> = ({ consortium }) =
 
     const isMember = consortium.members.some(member => member.id === userId);
 
-    const consortiumJoin = async ({ consortiumId }: { consortiumId: string }) => {
-
-    }
-
-    const consortiumLeave = async ({ consortiumId }: { consortiumId: string }) => {
-
-    }
-
+    const { consortiumJoin, consortiumLeave } = useCentralApi();
 
     // Handle joining the consortium
     const handleJoin = async () => {
         setLoading(true);
         try {
             await consortiumJoin({ consortiumId: consortium.id });
-            // You can refetch or update the UI state to reflect the change
         } catch (error) {
             console.error("Failed to join the consortium:", error);
         } finally {
             setLoading(false);
+            onReload();
         }
     };
 
@@ -49,11 +44,12 @@ const ConsortiumListItem: React.FC<ConsortiumListItemProps> = ({ consortium }) =
             console.error("Failed to leave the consortium:", error);
         } finally {
             setLoading(false);
+            onReload();
         }
     };
 
     return (
-        <Box display="flex" flexDirection="row" style={{background: 'white', padding: '1rem', marginBottom: '1rem'}}>
+        <Box display="flex" flexDirection="row" alignItems="center" style={{background: 'white', padding: '1rem', marginBottom: '1rem'}}>
             <Box flex={1}>
                 <a onClick={() => navigate(`/consortium/details/${consortium.id}`)}>
                     <Typography variant="h6">{consortium.title || 'No Title'}</Typography>
@@ -61,9 +57,18 @@ const ConsortiumListItem: React.FC<ConsortiumListItemProps> = ({ consortium }) =
                 <Typography>{consortium.description || 'No Description'}</Typography>
             </Box>
             <Box>
-                <Button
+                {isMember && <Button
                     variant="contained"
-                    color={isMember ? "secondary" : "primary"}
+                    color="success"
+                    onClick={() => navigate(`/consortium/details/${consortium.id}`)}
+                    disabled={loading}
+                    sx={{ ml: 2 }}
+                >
+                    View
+                </Button>}
+                <Button
+                    variant={isMember ? "outlined" : "contained"}
+                    color="primary"
                     onClick={isMember ? handleLeave : handleJoin}
                     disabled={loading}
                     sx={{ ml: 2 }}
