740cb83e9538d61282327caf94de2ed9be01b4d3
Dylan Martin <dylanrossmartin@gmail.com>
2024-11-01 11:54:51 -0400

    fixed app load

--- desktopApp/electronApp/src/preload.js
+++ desktopApp/electronApp/src/preload.js
@@ -2,8 +2,10 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('ElectronAPI', {
   getConfig: async () => ipcRenderer.invoke('getConfig'),
-  openConfig: async (filePath) => ipcRenderer.invoke("openConfig"),
+  openConfig: async () => ipcRenderer.invoke("openConfig"),
   applyDefaultConfig: async () => ipcRenderer.invoke("applyDefaultConfig"),
   getConfigPath: async () => ipcRenderer.invoke("getConfigPath"),
   useDirectoryDialog: async (pathString) => ipcRenderer.invoke("useDirectoryDialog", pathString),
 });
+
+console.log("preload.js loaded");
