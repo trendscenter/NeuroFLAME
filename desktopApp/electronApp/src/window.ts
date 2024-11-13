08fa0c396b58f9e0fd910f83ef183bceaa402af8
Dylan Martin <dylanrossmartin@gmail.com>
2024-11-13 15:19:12 -0500

    ensure directories exist on initializing app (#38)
    * ensure directories exist
    
    * add file
    
    * save config with populated paths

--- desktopApp/electronApp/src/window.ts
+++ desktopApp/electronApp/src/window.ts
@@ -1,26 +1,35 @@
-import { BrowserWindow, session } from 'electron'
+import { BrowserWindow, session, app } from 'electron'
 import path from 'path'
+import url from 'url'
 
-export async function createMainWindow(
-  __dirname: string,
-): Promise<BrowserWindow> {
-  // TODO: handle sessions
+export async function createMainWindow(): Promise<BrowserWindow> {
+  const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
+  
+  // Window configuration
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    // frame: false,
-    // titleBarStyle: 'hidden',
-    // trafficLightPosition: { x: 25, y: 25 },
-    //fullscreen: true,
     webPreferences: {
-      nodeIntegration: false,
-      contextIsolation: true,
       preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
       session: session.fromPartition(Date.now().toString()),
     },
   })
 
-  // mainWindow.webContents.openDevTools()
+  // Define URLs for production and development
+  const appPath = app.getAppPath()
+  const productionPath = path.join(appPath, '../app/build/index.html')
+  const productionUrl = url.format({
+    pathname: productionPath,
+    protocol: 'file:',
+    slashes: true,
+  })
+  const developmentUrl = 'http://localhost:3000'
+  
+  // Choose the appropriate start URL
+  const startUrl = app.isPackaged ? productionUrl : developmentUrl
 
+  await mainWindow.loadURL(startUrl)
   return mainWindow
 }
