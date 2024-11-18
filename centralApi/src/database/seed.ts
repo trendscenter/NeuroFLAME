0b863c8b1244bd38d5b5cbdacb3e469ccfafb1ff
Dylan Martin <dylanrossmartin@gmail.com>
2024-11-18 14:55:06 -0500

    change seed

--- centralApi/src/database/seed.ts
+++ centralApi/src/database/seed.ts
@@ -48,7 +48,7 @@ const computations = [
         _id: predefinedIds.computation1Id,
         title: 'NVFLARE boilerplate average',
         imageName: 'dylanrmartin/computations:boilerplate_average_app-9a51878c2ef13180187867d20b3361c516891d11',
-        imageDownloadUrl: 'dylanrmartin/computations:boilerplate_average_app-9a51878c2ef13180187867d20b3361c516891d11',
+        imageDownloadUrl: 'docker pull dylanrmartin/computations:boilerplate_average_app-9a51878c2ef13180187867d20b3361c516891d11',
         notes: computationNotesNvflareBoilerplate,
         owner: predefinedIds.user1Id.toString(),
     },
