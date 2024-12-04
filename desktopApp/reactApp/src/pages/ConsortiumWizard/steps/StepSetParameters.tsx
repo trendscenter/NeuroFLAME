ed1035c2794e1b8bfea51d12f7e0f3a63f8679bf
Dylan Martin <dylanrossmartin@gmail.com>
2024-12-04 14:59:21 -0500

    changes

---
+++ desktopApp/reactApp/src/pages/ConsortiumWizard/steps/StepSetParameters.tsx
@@ -0,0 +1,7 @@
+import ComputationParameters from "../../ConsortiumDetails/ComputationParameters/ComputationParameters";
+
+export default function StepSetParameters() {
+    return <div>
+        <ComputationParameters />
+    </div>
+}
