e8daf0bead2e06a873d3fc1ca9a0942d2b37a84a
Javier Romero <javiertomasromero@gmail.com>
2025-02-10 19:02:00 -0700

    Adding "App Config" and "Logout" links

--- desktopApp/reactApp/src/components/NavDrawer.tsx
+++ desktopApp/reactApp/src/components/NavDrawer.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Drawer, List, ListItem, ListItemText } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { useUserState } from '../contexts/UserStateContext';
+import { useNavigate } from "react-router-dom";
 
 interface NavDrawerProps {
   open: boolean;
@@ -10,13 +11,28 @@ interface NavDrawerProps {
 }
 
 const NavDrawer: React.FC<NavDrawerProps> = ({ open, onClose, navSetDrawerOpen }) => {
-  const { roles } = useUserState();
+  const { roles, username, clearUserData } = useUserState();
 
   const isAdmin = roles.includes('admin');
 
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    clearUserData();
+    navSetDrawerOpen(false);
+    navigate("/"); // React Router navigation
+  };
+
   return (
-    <Drawer anchor="right" open={open} onClose={onClose}>
-      <List>
+    <Drawer
+      anchor="right"
+      open={open}
+      onClose={onClose}
+      PaperProps={{
+        sx: { backgroundColor: "#001f70", color: "white" },
+      }}
+    >
+      <List sx={{ "& a": { textDecoration: "none", color: "white" } }}>
         <ListItem onClick={() => { navSetDrawerOpen(false) }} component={Link} to="/home">
           <ListItemText primary="Home" />
         </ListItem>
@@ -36,6 +52,17 @@ const NavDrawer: React.FC<NavDrawerProps> = ({ open, onClose, navSetDrawerOpen }
             </ListItem>
           )
         }
+      </List>
+      <List 
+        style={{position: 'absolute', bottom: 0, borderTop: '1px solid white', width: '100%', background: '#efefef'}} 
+        sx={{ "& a": { textDecoration: "none", color: "#001f70" } }}
+      >
+        <ListItem onClick={() => { navSetDrawerOpen(false) }} component={Link} to="/appConfig">
+          <ListItemText primary="User Config" />
+        </ListItem>
+        <ListItem onClick={() => { handleLogout() }} style={{cursor: 'pointer'}}>
+          <ListItemText primary="Logout" sx={{ color: "#001f70" }} />
+        </ListItem>
       </List>
     </Drawer>
   );
